3f9ed35d6cf4983193183b6909d1b02c0843a27c
mayone-du <cocomayo1201@icloud.com>
2022-04-23 17:21:22 +0900

    fix: token validate

--- src/auth/validateToken.ts
+++ src/auth/validateToken.ts
@@ -8,6 +8,7 @@ export const validateToken = (targetToken: string) => {
   if (bearer !== "Bearer") throw Error("Bearer is not found");
   const payload = decode(token);
   if (typeof payload !== "object" || !payload) throw Error("invalid token");
-  if (payload.exp && payload.exp * 1000 < Date.now()) throw Error("token expired");
+  // TODO: トークンの有効期限を延長する このままだと5分とかしか無理
+  // if (payload.exp && payload.exp * 1000 < Date.now()) throw Error("token expired");
   return { payload: payload as Payload };
 };
